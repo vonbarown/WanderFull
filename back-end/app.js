4dc4a151b94845a6f869dbd81f034cd90f8be629
vonbarown <vonielbrown@pursuit.org>
2020-01-05 12:39:24 -0500

    starting on routes

--- back-end/app.js
+++ back-end/app.js
@@ -22,8 +22,9 @@ const upload = multer({
 
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const uploadRouter = require('./routes/upload')
+const usersRouter = require('./routes/Users/users');
+const photosRouter = require('./routes/Photos/photo')
+
 
 const app = express();
 
@@ -39,7 +40,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.post('/upload', upload.single('image'), uploadRouter)
+app.use('/images', upload.single('image'), photosRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
