605eaf6d687a3d14a91569d9e308690e148d51cb
vonbarown <vonielbrown@pursuit.org>
2020-01-05 15:10:05 -0500

    basic database request for images

--- back-end/routes/Photos/photo.js
+++ back-end/routes/Photos/photo.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET home page. */
-router.post('/', (req, res, next) => {
-    console.log('req.file', req.file)
+const { db } = require('../../database/databasejs')
 
+
+const addPic = async (req, res, next) => {
+    console.log('req.file', req.file)
     try {
         let imageUrl = "http://localhost:8080/" + req.file.path.replace('public/', '')
+        let insertQuery = `INSERT INTO posts (user_id, img, caption,hashtag) 
+        VALUES($/user_id/, $/imageUrl/,$/caption/,$/hashtag/)`
+
+        let bodyCopy = Object.assign({}, req.body)
+        bodyCopy.imageUrl = imageUrl
+
+        await db.none(insertQuery, bodyCopy)
 
         res.json({
             message: 'image uploaded',
@@ -14,18 +22,22 @@ router.post('/', (req, res, next) => {
         })
     } catch (error) {
         console.log(error)
-
-
-
     }
+}
+router.post('/', addPic)
 
-})
+const getPics = async (req, res, next) => {
 
-router.get('/', (req, res, next) => {
-    // console.log('req.file', req.file)
-    res.send('hello')
+    let pictures = await db.any('SELECT * FROM posts')
 
-})
+    res.json({
+        status: 'success',
+        message: 'retrieved all post',
+        payload: pictures
+    })
+}
+
+router.get('/', getPics)
 
 
 module.exports = router;
