2dff75e21f627cd0ab60d9096cfa0094d9f2ec38
Jenesh <jenesh.napit@gmail.com>
2020-01-05 12:36:42 -0500

    Finished connecting img upload test component

--- front-end/src/pages/TestPage.jsx
+++ front-end/src/pages/TestPage.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const TestPage = () => {
-    const uploadImg = async (e) => {
-        e.preventDefault()
-        console.log('Upload Requested')
-        console.dir(e.target)
+class TestPage extends Component {
+    state = {
+        imgFile: null,
+        imgUrl: ''
     }
 
-    return (
-        <div>
-            <form onSubmit={uploadImg}>
-                <input type="file" />
-                <button>Upload</button>
-            </form>
-        </div>
-    )
+    uploadImg = async (e) => {
+        e.preventDefault()
+        console.log('Upload Requested')
+
+        const file = new FormData()
+        file.append('image', this.state.imgFile)
+
+        try {
+            const data = await axios.post('http://localhost:8080/upload', file)
+            console.log(data.data)
+            this.setState({imgUrl: data.data.imageUrl})
+            console.log('Upload Successul')
+        } catch (err) {
+            console.error(err)
+            console.log('Upload Unsuccessful')
+        }
+    }
+
+    changeFile = (e) => {
+        console.log('File Updated')
+        console.dir(e.target)
+        this.setState({imgFile: e.target.files[0]})
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.uploadImg}>
+                    <input type="file" onChange={this.changeFile} />
+                    <input type="submit" value="Upload" />
+                </form>
+                {this.state.imgUrl ? <img src={this.state.imgUrl} alt="uploaded" /> : null}
+            </div>
+        )
+    }
 }
 
 export default TestPage
