2b4aaf2def1aee2b29a6c311a1a5df5ad920ecc2
vonbarown <vonielbrown@pursuit.org>
2020-01-06 11:59:40 -0500

    testing carousel

--- front-end/src/Components/Landing/Carousel.jsx
+++ front-end/src/Components/Landing/Carousel.jsx
@@ -1,36 +1,36 @@
 import React, { Component } from 'react'
 
 class imgCarousel extends Component {
-    constructor(){
-    super()
-    this.state = {
-        index: 0,
-        array: ["http://packages.world-avenues.com/wp-content/uploads/2017/10/nightlife-singapore.jpg.jpg", 
+    constructor() {
+        super()
+        this.state = {
+            index: 0,
+            array: ["http://packages.world-avenues.com/wp-content/uploads/2017/10/nightlife-singapore.jpg.jpg",
                 "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg/1200px-001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg",
                 "http://rmafoodjourney.weebly.com/uploads/5/2/1/5/52157521/1175557_orig.jpg",
                 "https://d36tnp772eyphs.cloudfront.net/blogs/1/2019/08/Venezuelan-arepas.jpg"]
+        }
     }
-}
 
-    componentDidMount(){
-    this.interval = setInterval(() => {
-      const {array, index} = this.state
-      const newIndex = (index + 1) % (array.length)
-      this.setState({
-          index: newIndex
-      })
-    }, 3000);
-}
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            const { array, index } = this.state
+            const newIndex = (index + 1) % (array.length)
+            this.setState({
+                index: newIndex
+            })
+        }, 3000);
+    }
 
     render() {
-        return(
+        return (
             <div>
-               <img src={this.state.array[this.state.index]}/>
+                <img alt='carousel' src={this.state.array[this.state.index]} />
             </div>
         )
     }
 }
-    
+
 
 
 export default imgCarousel
