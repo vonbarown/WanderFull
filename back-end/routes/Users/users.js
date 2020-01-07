65e9ccdf639bb9513fd9c417a52dea9f90d7b485
tbriany <brianytaveras@pursuit.org>
2020-01-07 16:58:56 -0500

    started users patch request

--- back-end/routes/Users/users.js
+++ back-end/routes/Users/users.js
@@ -65,11 +65,36 @@ router.get('/username', getUser)
 
 const updateUserInfo = async (req, res, next) => {
   // username, password, firstname, lastname, email, profile_pic
+  let user_id = req.body.user_id
+  let username = req.body.username
+  let password = req.body.password
+  let firstname = req.body.firstname
+  let lastname = req.body.lastname 
+  let email = req.body.email
+  let profile_pic = req.body.profile_pic 
 
-
+  try {
+    if (username) {
+      let updatedUsername = await db.any("UPDATE users SET username = $1 WHERE id = $2 RETURNING *", [username, user_id])
+      res.status(200)
+      res.json({
+        payload: updatedUsername,
+        message: `Success. Updated ${username} in users table.`
+      });
+    } else if(password) {
+      let updatedPassword = await db.any("UPDATE users SET password = $1 WHERE id = $2 RETURNING *", [password, user_id])
+      res.status(200)
+      res.json({
+        payload: updatedPassword,
+        message: `Success. Updated ${password} in users table.`
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-router.patch('/:username', updateUserInfo)
+router.patch('/', updateUserInfo)
 
 
 module.exports = router;
