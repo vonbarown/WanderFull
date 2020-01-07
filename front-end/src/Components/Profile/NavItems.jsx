13df8a27842b46b3e3895003b4ad6d1435c11afe
Suzette <suzetteislam@gmail.com>
2020-01-06 21:26:16 -0500

    gets data from db

--- front-end/src/Components/Profile/NavItems.jsx
+++ front-end/src/Components/Profile/NavItems.jsx
@@ -1,25 +1,12 @@
 import React, { Component } from 'react'
-import Button from '../Shared/Button'
 
 class NavBtns extends Component {
     render() {
-        return (<div className='navBtn'>
-            <Button
-                value='Travel'
-            />
-
-            <Button
-                value='Location'
-            />
-
-            <Button
-                value='Wishlist'
-            />
-
-            <Button
-                value='Buds'
-            />
-        </div>)
+        return (
+        <div className='navBtn'>
+      
+        </div>
+        )
     }
 }
 
