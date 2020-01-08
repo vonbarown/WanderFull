b4c81fa89c4bbef95c1f6320b5be2d0106fbc8bc
vonbarown <vonielbrown@pursuit.org>
2020-01-08 12:13:59 -0500

    adding hamburger to profile

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import UserAvatar from '../Components/Shared/Avatar'
 import NavBar from '../Components/Profile/NavBar'
 import ImageCard from '../Components/Shared/Cards'
+import Hamburger from '../Components/Shared/Hamburger'
+import { Container } from '@material-ui/core'
 import '../styles/profile.css'
 class Profile extends Component {
 
@@ -38,7 +40,16 @@ class Profile extends Component {
 
                     username: "Voniel"
                 }
+                ,
+                {
+                    caption: "Japanese street food",
 
+                    hashtag: "#japan",
+
+                    img: "https://3tsll33cscvk11pae33oze51-wpengine.netdna-ssl.com/wp-content/uploads/2018/01/tokyo-street-food-takoyaki.png",
+
+                    username: "Voniel"
+                }
             ]
         }
     }
@@ -74,16 +85,18 @@ class Profile extends Component {
         return (
             <div className='profile'>
                 <div className='header'>
-                    <UserAvatar pic={'https://media.newyorker.com/photos/5e06335ca15be900089fe632/master/pass/Brody-CatsReview.jpg'} class_name={'classes.large'} userName={"Test"} />
+                    <div className='top'>
+                        <UserAvatar pic={'https://media.newyorker.com/photos/5e06335ca15be900089fe632/master/pass/Brody-CatsReview.jpg'} class_name={'classes.large'} userName={"Test"} />
+                        <Hamburger />
+                    </div>
                     <NavBar />
-
-                    {
-                        album.map(el => {
-                            return <ImageCard postPic={el.img} pic={'https://media.newyorker.com/photos/5e06335ca15be900089fe632/master/pass/Brody-CatsReview.jpg'} />
-                        })
-
-                    }
                 </div>
+                {
+                    album.map(el => {
+                        return <ImageCard postPic={el.img} pic={'https://media.newyorker.com/photos/5e06335ca15be900089fe632/master/pass/Brody-CatsReview.jpg'} />
+                    })
+
+                }
             </div>
         )
     }
