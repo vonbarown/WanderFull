64a64686c927699247692f75dcb4ba58447d3d1b
vonbarown <vonielbrown@pursuit.org>
2020-01-07 21:19:04 -0500

    adding material ui avatar component

--- front-end/src/Components/Shared/Avatar.jsx
+++ front-end/src/Components/Shared/Avatar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 
-const UserAvatar = (props) => (
-    <div className='userAvatar'>
-        <img className='userPic' src={props.pic} alt="user" />
-        <p>{props.userName}</p>
-    </div>
-)
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+    },
+    large: {
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+    },
+}));
 
-export default UserAvatar
+export default function UserAvatars(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root, 'userAvatar'}>
+            <Avatar alt="user" src={props.pic} className={classes.large} />
+            <p>{props.userName}</p>
+        </div>
+    );
+}
+
+
+
