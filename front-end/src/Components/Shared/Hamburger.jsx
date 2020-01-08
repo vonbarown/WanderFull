5ca4758cdc0c3db988bab7d427b78a2ebdb12b9a
Suzette <suzetteislam@gmail.com>
2020-01-07 21:50:14 -0500

    new menu

--- front-end/src/Components/Shared/Hamburger.jsx
+++ front-end/src/Components/Shared/Hamburger.jsx
@@ -1,41 +1,69 @@
-import React from 'react'
-import Home from '../../pages/Home'
-import '../../styles/Hamburger.css'
-import Profile from '../../pages/Profile'
-import Settings from '../../pages/Settings'
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Home } from '../../pages/Home'
+import { Profile } from '../../pages/Profile'
+import { Settings } from '../../pages/Settings'
+// import '../../styles/Hamburger.css'
 
 
-const feedMenu = () => {
+const options = [
+    <a href = '/home'>Home</a>,
+    <a href = '/profile'>Profile</a>,
+    <a href = '/settings'>Settings</a>,
+    <a href ='/search'>Search</a>
+];
+
+const ITEM_HEIGHT = 48;
+
+const Hamburger = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget)
+
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    
     return (
-        <ul>
-            <li><a href='/profile' component={Profile}> Profile </a></li>
-            <li><a href='/settings' component={Settings} > Settings</a></li>
-            <li><input type='text' /></li>
-        </ul>
-    )
+        <div>
+            <IconButton
+                aria-label="more"
+                aria-controls="long-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <Menu
+                id="long-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: 200,
+                    },
+                }}
+            >
+                {options.map(option => (
+                    <MenuItem key={option} selected={option === 'Search'} onClick={handleClose}>
+                        {option}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
+    );
 }
-
-const profileMenu = () => {
-    return (
-        <ul>
-            <li><a href='/home' component={Home}>Feed</a></li>
-            <li><a href='/settings' component={Settings}> Settings</a></li>
-        </ul>
-    )
-}
-
-const Hamburger = ({ feed }) => (
-    <header className='hamburger'>
-        <nav className='hamburger-nav'>
-            <div></div>
-            <div>
-                {feed ? feedMenu() : profileMenu()}
-
-            </div>
-        </nav>
-    </header>
-
-)
 
 
 export default Hamburger
