dd1a15a24eba4419da64ed40e6da36e653e52536
Suzette <suzetteislam@gmail.com>
2020-01-07 21:08:45 -0500

    need to commit

--- front-end/src/Components/Shared/Hamburger.jsx
+++ front-end/src/Components/Shared/Hamburger.jsx
@@ -1,32 +1,35 @@
 import React from 'react'
-// import Home from '../../pages/Home'
+import Home from '../../pages/Home'
 import '../../styles/Hamburger.css'
+import Profile from '../../pages/Profile'
+import Settings from '../../pages/Settings'
 
 
 const feedMenu = () => {
-  return  (
+    return (
         <ul>
-            <li><a href='/profile'> Profile </a></li>
-            <li><a href='/settings'> Settings</a></li>
-            <li><input type = 'text'/></li>
-        </ul>    
-)
+            <li><a href='/profile' component={Profile}> Profile </a></li>
+            <li><a href='/settings' component={Settings} > Settings</a></li>
+            <li><input type='text' /></li>
+        </ul>
+    )
 }
 
 const profileMenu = () => {
     return (
-    <ul>
-        <li><a href = '/home'>Feed</a></li>
-        <li><a href = '/settings'> Settings</a></li>
-    </ul>
-)} 
+        <ul>
+            <li><a href='/home' component={Home}>Feed</a></li>
+            <li><a href='/settings' component={Settings}> Settings</a></li>
+        </ul>
+    )
+}
 
 const Hamburger = ({ feed }) => (
-    <header className = 'hamburger'>
-        <nav className = 'hamburger-nav'>
+    <header className='hamburger'>
+        <nav className='hamburger-nav'>
             <div></div>
             <div>
-                {feed ? feedMenu() : profileMenu() }
+                {feed ? feedMenu() : profileMenu()}
 
             </div>
         </nav>
--- front-end/src/pages/Settings.jsx
+++ front-end/src/pages/Settings.jsx
@@ -1,1 +1,10 @@
 import React from 'react'
+
+const Settings = () =>{
+    return (
+        <div>Settings</div>
+    )
+
+}
+
+export default Settings
