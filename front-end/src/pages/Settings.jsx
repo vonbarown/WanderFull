84c130f62667b2a09150c0ada78fe07a1d362c38
vonbarown <vonielbrown@pursuit.org>
2020-01-08 20:58:46 -0500

    installing styled components

--- front-end/src/pages/Settings.jsx
+++ front-end/src/pages/Settings.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../styles/settings.css'
 
 
+class Settings extends Component {
 
+    handleSubmit = (event) => {
+        // make patch network request to /users endpoint
+        event.preventDefault()
+        console.log('form was submitted')
+    }
 
-const handleSubmit = (event) => {
-    // make patch network request to /users endpoint
-    // event.preventDefault()
-    console.log('form was submitted')
-}
-
-const Settings = () => {
-    return (
-        <div className='settingsPage'>
-            <h1>Settings</h1>
-            <p>Dark Theme<button>on/off</button></p>
-            <p>Edit Profile info</p>
-            <form onSubmit={handleSubmit()}>
-                <input type="text" placeholder="username"></input>
-                <input type="text" placeholder="profile pic url"></input>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+    render() {
+        return (
+            <div className='settingsPage'>
+                <h1>Settings</h1>
+                <p>Dark Theme<button>on/off</button></p>
+                <p>Edit Profile info</p>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="username"></input>
+                    <input type="text" placeholder="profile pic url"></input>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Settings 
