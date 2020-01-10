6c5adc538be2fe338513d010746c92644f4988b2
vonbarown <vonielbrown@pursuit.org>
2020-01-10 08:49:33 -0500

    warnings clean up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from './themes/theme'
 import { GlobalStyles } from './themes/global'
-import Toggle from './themes/Toggler'
 import Landing from './Components/Landing/Landing'
 import { useDarkMode } from './themes/useDarkMode'
 
 const App = () => {
-    const [theme, toggleTheme, componentMounted] = useDarkMode();
+    const [theme, componentMounted] = useDarkMode();
     const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
     if (!componentMounted) {
--- front-end/src/pages/Settings.jsx
+++ front-end/src/pages/Settings.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Hamburger from '../Components/Shared/Hamburger'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from '../themes/theme'
