f5e8acb2f5c614b7d5a0efb5b4453f23c30df881
vonbarown <vonielbrown@pursuit.org>
2020-01-11 18:43:05 -0500

    styling profile page

--- front-end/src/Components/Shared/Avatar.jsx
+++ front-end/src/Components/Shared/Avatar.jsx
@@ -9,10 +9,6 @@ const useStyles = makeStyles(theme => ({
             margin: theme.spacing(1),
         },
     },
-    small: {
-        width: theme.spacing(3),
-        height: theme.spacing(3),
-    },
     large: {
         width: theme.spacing(25),
         height: theme.spacing(25),
