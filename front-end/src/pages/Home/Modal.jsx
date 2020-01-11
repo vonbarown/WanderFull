fd91a67cbc619b9dcaabf7de2ca21f91bd0d51c2
vonbarown <vonielbrown@pursuit.org>
2020-01-11 17:07:04 -0500

    modal button fixed to the bottom right

--- front-end/src/pages/Home/Modal.jsx
+++ front-end/src/pages/Home/Modal.jsx
@@ -27,11 +27,15 @@ const useStyles = makeStyles(theme => ({
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
     },
+    root: {
+        position: 'fixed',
+        right: '50px',
+        top: '750px'
+    }
 }));
 
 export default function UploadModal() {
     const classes = useStyles();
-    // getModalStyle is not a pure function, we roll the style only on the first render
     const [modalStyle] = React.useState(getModalStyle);
     const [open, setOpen] = React.useState(false);
 
@@ -44,13 +48,13 @@ export default function UploadModal() {
     };
 
     return (
-        <div>
-            <button type="button" onClick={handleOpen}>
+        <div className='modal'>
+            <button type="button" onClick={handleOpen} className={classes.root}>
                 Open Modal
       </button>
             <Modal
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
+                aria-labelledby="Upload post"
+                aria-describedby="Upload user post"
                 open={open}
                 onClose={handleClose}
             >
