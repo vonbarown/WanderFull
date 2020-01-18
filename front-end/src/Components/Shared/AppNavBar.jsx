87b4ae148001597a9a85f6a5ba839a6272de89d3
vonbarown <vonielbrown@pursuit.org>
2020-01-18 00:59:20 -0500

    placing logo in navbar

--- front-end/src/Components/Shared/AppNavBar.jsx
+++ front-end/src/Components/Shared/AppNavBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hamburger from './Hamburger'
 import '../../styles/AppNavBar.css'
+import logo from '../../themes/Logo/f537d019-e1b6-4e42-8275-2c9c5c7b8075_200x200.png'
 
 const AppNavBar = (props) => {
     return (
         <div className='appNavBar'>
             <div className='appNavBarItems'>
-                <h1>WanderFull</h1>
+                <div className='logo'>
+                    <img src={logo} alt='logo' />
+                    <h3>WanderFull</h3>
+                </div>
                 <Hamburger />
             </div>
             {props.children}
--- front-end/src/pages/Settings.jsx
+++ front-end/src/pages/Settings.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from '../themes/theme'
 import { GlobalStyles } from '../themes/global'
 import Toggle from '../themes/Toggler'
 import { useDarkMode } from '../themes/useDarkMode'
+// import { useForm } from 'react-hook-form'
+import axios from 'axios'
 import '../styles/settings.css'
 
 
@@ -12,14 +14,31 @@ const Settings = () => {
     const [theme, toggleTheme, componentMounted] = useDarkMode();
     const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
+    const [data, setData] = useState({ hits: [] })
+    const [query, setQuery] = useState('1');
+    // const { register, handleSubmit, watch, errors } = useForm();
+
+    const fetchData = async () => {
+        const result = await axios(
+            `http://localhost:8080/posts/profile/${Number(query)}`,
+        );
+        setData(result.data);
+    }
+
+    const onSubmit = data
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+    console.log(data);
+
+
     if (!componentMounted) {
         return <div />
     }
-    const handleSubmit = (event) => {
-        // make patch network request to /users endpoint
-        // event.preventDefault()
-        console.log('form was submitted')
-    }
+
 
     // render() {
     return (
@@ -32,8 +51,8 @@ const Settings = () => {
                     <Toggle theme={theme} toggleTheme={toggleTheme} />
                 </div>
                 <p>Edit Profile info</p>
-                <form onSubmit={handleSubmit()}>
-                    <input type="text" placeholder="username"></input>
+                <form onSubmit={e => e.preventDefault()} >
+                    <input type="text" placeholder="username" onChange={e => setQuery(e.target.value)}></input>
                     <input type="text" placeholder="profile pic url"></input>
                     <button>Submit</button>
                 </form>
