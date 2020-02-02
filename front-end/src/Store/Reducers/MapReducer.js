1f02011fd70acd0dd34cc4e54dd7e132fe5432fc
vonbarown <vonielbrown@pursuit.org>
2020-02-02 06:32:23 -0500

    initailizing a redux state

---
+++ front-end/src/Store/Reducers/MapReducer.js
@@ -0,0 +1,20 @@
+import { LOAD_MAP } from "../Actions/actionTypes";
+
+const initialState = {
+    stores: []
+};
+
+export const mapReducer = (state = initialState, action) => {
+
+    let stateCopy = { ...state }
+
+    switch (action.type) {
+        case LOAD_MAP:
+            let newMarker = action.payload
+
+            stateCopy.stores = [...stateCopy.stores, newMarker]
+            break
+        default:
+            return stateCopy;
+    }
+};
---
+++ front-end/src/Store/Reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { mapReducer } from './MapReducer'
+
+export const rootReducer = combineReducers({
+    mapReducer,
+});
+
