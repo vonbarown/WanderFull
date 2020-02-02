b8c329f0dc748ef58ed352b4966d2dab8bded475
vonbarown <vonielbrown@pursuit.org>
2020-02-02 14:49:21 -0500

    fixed syntax error

--- front-end/src/Store/Reducers/rootReducer.js
+++ front-end/src/Store/Reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { mapReducer } from './mapReducer'
+import { mapReducer } from './MapReducer'
 
 export const rootReducer = combineReducers({
     mapReducer,
