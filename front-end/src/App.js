16df9ecd338b73677f034e504984cdca74056147
vonbarown <vonielbrown@pursuit.org>
2020-05-02 17:40:14 -0400

    new theme toggle

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,43 +1,141 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import { lightTheme, darkTheme } from './themes/theme'
-import { GlobalStyles } from './themes/global'
-import Landing from './Components/Landing/Landing'
-import { useDarkMode } from './themes/useDarkMode'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import Toggle from "react-toggle";
+import Container from "./Components/Landing/Container";
+import { Switch, Route } from "react-router-dom";
+import Feed from "./pages/Feed/Feed";
+import Profile from "./pages/Profile";
+import { Buds } from "./Components/Profile/Buds";
+import Settings from "./pages/Settings";
+import AppNavBar from "./Components/Shared/AppNavBar";
+import Home from "./pages/Home/Home";
+import "./App.css";
 
-const App = () => {
-    const [theme, componentMounted] = useDarkMode();
-    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+import Upload from "./Components/TestComponents/UploadForm";
 
-    const [trending, setTrending] = useState([])
+import MapContainer from "./Components/Map/MapOfficial";
 
-    const fetchTrending = async () => {
-        try {
-            const { data } = await axios.get('api/likes/posts/popular')
-            setTrending(data)
+class Landing extends Component {
+  state = {
+    firstRender: false,
+    login: true,
+    darkMode: false,
+    toggle: false,
+  };
 
-        } catch (error) {
-            console.log(error);
-        }
+  componentDidMount() {
+    //add eventListener to check for the users preferred device
+    window.addEventListener("scroll", this.handleScroll);
+
+    const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+    this.handleThemeToggle(darkPref.matches);
+
+    if (darkPref.matches) {
+      console.log(darkPref);
+      this.setState((prevState) => {
+        return {
+          darkMode: !prevState.darkMode,
+          toggle: !prevState.toggle,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          toggle: !prevState.toggle,
+        };
+      });
     }
 
-    console.log(trending);
-    useEffect(() => {
-        fetchTrending()
-    }, [])
+    //creating interval to change class name of the scroll down arrow
+    // setInterval(() => {
+    //   this.setState({ blink: !this.state.blink });
+    // }, 2000);
+  }
 
-    if (!componentMounted) {
-        return <div />
+  handleThemeToggle = (e) => {
+    // this.setState({ darkMode: e });
+    if (e) {
+      document.body.style.backgroundColor = "black;";
+    } else {
+      document.body.style = "background: white-smoke; transition: 0.5s ease;";
     }
+  };
+
+  handleChange = (event) => {
+    const { firstRender } = this.state;
+    console.log(event.target.value);
+    console.log(event.target.innerText);
+
+    if (firstRender) {
+      this.switchFirstRender();
+    }
+
+    if (event.target.innerText.includes("Login")) {
+      this.setState({
+        login: true,
+      });
+    } else {
+      this.setState({
+        login: false,
+      });
+    }
+  };
+
+  handleLogout = () => {
+    console.clear();
+    console.log("Logout");
+    console.log(this.props);
+    // this.props.history.push('/asdf')
+  };
+
+  switchFirstRender = () => {
+    this.setState({
+      firstRender: false,
+    });
+  };
+
+  renderContainer = (routeProps) => {
+    // console.log(routeProps)
+    const { firstRender, login, signUp } = this.state;
+    const { handleChange, submitLogin, handleLogout } = this;
+    return (
+      <Container
+        id="container"
+        submitLogin={submitLogin}
+        handleChange={handleChange}
+        firstRender={firstRender}
+        login={login}
+        signUp={signUp}
+        handleLogout={handleLogout}
+      />
+    );
+  };
+
+  render() {
+    console.log("Landing props:", this.props);
+    const { darkMode } = this.state;
 
     return (
-        <ThemeProvider theme={themeMode} >
-            <GlobalStyles />
-            <Landing />
-        </ThemeProvider >
-    )
+      <div className={darkMode ? "darkMode" : "App"}>
+        <Switch>
+          <Route path="/home" component={Home} />
+          {!!sessionStorage.getItem("user") && (
+            <>
+              <Route path="/buds" component={Buds} />
+              <Route path="/upload" component={Upload} />
+              <Route path="/map" component={MapContainer} />
+
+              <AppNavBar>
+                <Route path="/feed" component={Feed} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/settings" component={Settings} />
+              </AppNavBar>
+            </>
+          )}
+          <Route path="/" component={this.renderContainer} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App
+export default Landing;
--- front-end/src/pages/Settings.jsx
+++ front-end/src/pages/Settings.jsx
@@ -1,97 +1,82 @@
-import React, { useState } from 'react'
-import { ThemeProvider } from 'styled-components'
-import { lightTheme, darkTheme } from '../themes/theme'
-import { GlobalStyles } from '../themes/global'
-import Toggle from '../themes/Toggler'
-import { useDarkMode } from '../themes/useDarkMode'
-import '../styles/settings.css'
-import { Button } from '@material-ui/core'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import "../styles/settings.css";
+import { Button } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = {
-    display: 'none',
-}
+  display: "none",
+};
 
 const Settings = () => {
+  const [data, setData] = useState({ data: [] });
+  const [username, setUsername] = useState("");
 
-    const [theme, toggleTheme, componentMounted] = useDarkMode();
-    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  const fetchData = async () => {
+    const {
+      data: { payload },
+    } = await axios.patch(
+      `http://localhost:8080/users/update/${sessionStorage.getItem("user_id")}`,
+      {
+        username: username,
+      }
+    );
+    sessionStorage.setItem("user", payload.username);
+    setData(payload);
+  };
 
-    const [data, setData] = useState({ data: [] })
-    const [username, setUsername] = useState('');
+  const handleProfilePicUpdate = async (event) => {
+    event.preventDefault();
+    console.log("updating profile pic");
+    // let imageFile = event.target.files[0]
+    // const data = new FormData()
+    // data.append('profile_pic', imageFile)
+    // try {
+    //     const {data: {payload}} = await axios.post(`http://localhost:8080/users/update/`, data)
+    // } catch (error) {
 
+    // }
+  };
 
-    const fetchData = async () => {
-        const { data: { payload } } = await axios.patch(`http://localhost:8080/users/update/${sessionStorage.getItem('user_id')}`,
-            {
-                username: username
-            }
+  return (
+    <div className="settingsPage">
+      <h1>Settings</h1>
+      <div className="toggleButton">Change Theme</div>
 
-        );
-        sessionStorage.setItem('user', payload.username)
-        setData(payload);
-    }
+      <p>Edit Profile info</p>
+      <form
+        onSubmit={(e) => {
+          fetchData();
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        {
+          // <input type="text" placeholder="profile pic url"></input>
+        }
+        <button onClick={() => setUsername(username)}>Submit</button>
+      </form>
 
-    if (!componentMounted) {
-        return <div />
-    }
+      <form noValidate autoComplete="off" onSubmit={handleProfilePicUpdate}>
+        <input
+          accept="image/*"
+          id="contained-button-file"
+          multiple
+          type="file"
+          style={useStyles}
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" color="primary" component="span">
+            {" "}
+            Upload Photo{" "}
+          </Button>
+        </label>
+      </form>
+    </div>
+  );
+};
 
-    const handleProfilePicUpdate = async (event) => {
-        event.preventDefault()
-        console.log('updating profile pic')
-        // let imageFile = event.target.files[0]
-        // const data = new FormData()
-        // data.append('profile_pic', imageFile)
-        // try {
-        //     const {data: {payload}} = await axios.post(`http://localhost:8080/users/update/`, data)
-        // } catch (error) {
-
-        // }
-    }
-
-    return (
-
-        <div className='settingsPage'>
-            <ThemeProvider theme={themeMode} >
-                <GlobalStyles />
-            </ThemeProvider >
-
-            <h1>Settings</h1>
-            <div className='toggleButton'>
-                Change Theme
-                <Toggle theme={theme} toggleTheme={toggleTheme} />
-            </div>
-
-            <p>Edit Profile info</p>
-            <form onSubmit={e => {
-                fetchData()
-                e.preventDefault()
-            }} >
-                <input type="text" placeholder="username" onChange={e => setUsername(e.target.value)}></input>
-                {// <input type="text" placeholder="profile pic url"></input>
-                }
-                <button onClick={() => setUsername(username)}>Submit</button>
-            </form>
-
-            <form noValidate autoComplete="off" onSubmit={handleProfilePicUpdate}>
-                <input
-                    accept="image/*"
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                    style={useStyles}
-                />
-                <label htmlFor="contained-button-file">
-                    <Button variant="contained" color="primary" component="span"> Upload Photo </Button>
-                </label>
-            </form>
-
-            <div className="copyRight">Moon Icon made by<a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-                {' '} Sun Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-            </div>
-        </div>
-    )
-}
-
-export default Settings 
+export default Settings;
